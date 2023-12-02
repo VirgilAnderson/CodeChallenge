932f3ab6d58f690d61036dfc89e0b55ba1a158f8
Virgil Anderson <virgil.anderson@gmail.com>
2023-12-02 11:30:03 -0700

    Feat: Adds namespacing

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
