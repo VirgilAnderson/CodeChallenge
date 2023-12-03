92f5463761c8a977b7e8cd2b908d3c3a4bf6950b
Virgil Anderson <virgil.anderson@gmail.com>
2023-12-03 15:17:48 -0700

    Feat: Adds customer table styles

--- resources/js/Components/CustomerList.vue
+++ resources/js/Components/CustomerList.vue
@@ -31,19 +31,45 @@ export default {
             Load Customers
         </button>
 
-        <table v-if="customers.length" class="min-w-full leading-normal mt-6">
-            <thead>
-            <tr>
-
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="customer in customers" :key="customer.id">
-
-            </tr>
-            </tbody>
-        </table>
+        <div class="overflow-x-auto mt-6">
+            <table v-if="customers.length" class="min-w-full leading-normal">
+                <thead>
+                <tr>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Full Name
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Email
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Phone Number
+                    </th>
+                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Address
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="customer in customers" :key="customer.id" class="hover:bg-gray-100">
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {{ customer.firstName }} {{ customer.lastName }}
+                    </td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {{ customer.email }}
+                    </td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {{ customer.phone_number }}
+                    </td>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {{ customer.address }}
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <p v-else class="text-gray-700">No customers found.</p>
+        </div>
     </div>
 </template>
+
 
 <style></style>
