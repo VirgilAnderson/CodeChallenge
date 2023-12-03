5d45e136cbef7e55aab75aa6afb3850e3707b755
Virgil Anderson <virgil.anderson@gmail.com>
2023-12-03 15:33:31 -0700

    Feat: Adds form

--- resources/js/Components/Customer/Create.vue
+++ resources/js/Components/Customer/Create.vue
@@ -47,5 +47,22 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+    <div class="p-4">
+        <div v-if="successMessage" class="p-3 mb-4 bg-green-200 text-green-800 rounded">
+            {{ successMessage }}
+        </div>
+        <div v-if="errorMessage" class="p-3 mb-4 bg-red-200 text-red-800 rounded">
+            {{ errorMessage }}
+        </div>
+        <input v-model="firstName" type="text" placeholder="First Name" class="p-2 mb-4 border rounded w-full">
+        <input v-model="lastName" type="text" placeholder="Last Name" class="p-2 mb-4 border rounded w-full">
+        <input v-model="email" type="email" placeholder="Email" class="p-2 mb-4 border rounded w-full">
+        <input v-model="phoneNumber" type="tel" placeholder="Phone Number" class="p-2 mb-4 border rounded w-full">
+        <input v-model="address" type="text" placeholder="Address" class="p-2 mb-4 border rounded w-full">
+        <button @click="addCustomer" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Add Customer
+        </button>
+    </div>
+</template>
 <style></style>
